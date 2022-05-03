bb8543b568d6d3e323a4c554e7bb29bee62bff17
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-05-03 21:44:21 +0200

    Create ap.py

---
+++ ap.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('Image Captioning Group 13')
+
+with st.expander("The Team"):
+  st.write("Hello, we are Leon Lang, Jean Louis Fichtner and Loredana Bratu and we create this app as a part of our business informatics course ")
