d1fdcb774177b9430f384c699679a792cd891cdc
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-05-03 22:09:25 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -7,3 +7,7 @@ with st.expander("Our Team"):
   st.write("Hello! Our team consists of Frenki Pushaj, Benjamin Rattanpal & Patrik Spindler.")
   st.write("Our mission consists of creating an app that answers all your questions regarding celebrities.")
   st.write("We strive to achieve this by utilizing Machine Learning :)")
+  
+with col1:
+    st.header("Chrysler Logo")
+    st.image("https://drive.google.com/file/d/1YcyjUePcy3BYlic9eTY0Py-O2ekFn8MR/view?usp=sharing")
