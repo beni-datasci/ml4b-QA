d2d9050a3ff1d4a70de8e3f2ce45bd544a2b62b6
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-05-03 22:22:07 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 st.title('Question Answering')
  
-with st.expander("Our Team"):
+with st.expander("Our Team & Mission"):
   st.write("Hello! Our team consists of Frenki Pushaj, Benjamin Rattanpal & Patrik Spindler.")
   st.write("Our mission consists of creating an app that answers all your questions regarding celebrities.")
   st.write("We strive to achieve this by utilizing Machine Learning :)")
