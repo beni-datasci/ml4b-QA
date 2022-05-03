f7c8e35654316d65d3385d85bff9f4ad61cb7e38
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-05-03 21:54:36 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -6,4 +6,4 @@ st.title('Our Team')
 
 with st.expander("Our Team"):
   with st.expander("Benjamin Rattanpal"):
-  st.write("Hello, we are Leon Lang, Jean Louis Fichtner and Loredana Bratu and we create this app as a part of our business informatics course ")
+    st.write("Hello, we are Leon Lang, Jean Louis Fichtner and Loredana Bratu and we create this app as a part of our business informatics course ")
