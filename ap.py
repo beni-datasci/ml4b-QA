59dfe8d3cc4a17c1ee914880338c4da7faf01f67
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-05-03 23:59:41 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datasets import load_dataset
+dataset = load_dataset("squad_v2")
+
 st.title('Question Answering')
  
 with st.expander("Our Team & Mission"):
