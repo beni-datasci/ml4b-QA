918fe3bccbecc299ef770c0285c40aea45ade9eb
beni-datasci <104845399+beni-datasci@users.noreply.github.com>
2022-05-03 22:02:14 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Question Answering')
-st.title('Our Team')
  
 with st.expander("Our Team"):
   st.write("Hello! Our team consists of Frenki Pushaj, Benjamin Rattanpal & Patrik Spindler.")
